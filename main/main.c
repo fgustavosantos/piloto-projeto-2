129d091398e5239b33b1e3d563b188def9548d5f
fgustavosantos <fgustavo.santoss@gmail.com>
2026-08-28 23:37:07 -0300

    Update TWAI GPIO and bitrate configurations

--- main/main.c
+++ main/main.c
@@ -1,7 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2025-2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/param.h>
-#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "esp_log.h"
@@ -9,18 +14,16 @@
 #include "esp_twai.h"
 #include "esp_twai_onchip.h"
 
-#define TWAI_SENDER_TX_GPIO     19
-#define TWAI_SENDER_RX_GPIO     20
+#define TWAI_SENDER_TX_GPIO     GPIO_NUM_4
+#define TWAI_SENDER_RX_GPIO     GPIO_NUM_5
 #define TWAI_QUEUE_DEPTH        10
-#define TWAI_BITRATE            100000
+#define TWAI_BITRATE            250000
 
 // Message IDs
 #define TWAI_DATA_ID            0x100
 #define TWAI_HEARTBEAT_ID       0x7FF
-#define TWAI_DATA_LEN           100
-
-#define TWAI_SENSOR_1		0x123
-#define TWAI_SENSOR_2           0x456
+#define TWAI_EMERGENCY_ID       0x080
+#define TWAI_DATA_LEN           1000
 
 static const char *TAG = "twai_sender";
 
@@ -33,7 +36,7 @@ typedef struct {
 static IRAM_ATTR bool twai_sender_tx_done_callback(twai_node_handle_t handle, const twai_tx_done_event_data_t *edata, void *user_ctx)
 {
     if (!edata->is_tx_success) {
-        ESP_EARLY_LOGW(TAG, "Failed to transmit message, ID: 0x%" PRIx32, edata->done_tx_frame->header.id);
+        ESP_EARLY_LOGW(TAG, "Failed to transmit message, ID: 0x%X", edata->done_tx_frame->header.id);
     }
     return false; // No task wake required
 }
@@ -45,7 +48,8 @@ static IRAM_ATTR bool twai_sender_on_error_callback(twai_node_handle_t handle, c
     return false; // No task wake required
 }
 
-void app_main(void){
+void app_main(void)
+{
     twai_node_handle_t sender_node = NULL;
     printf("===================TWAI Sender Example Starting...===================\n");
 
@@ -77,35 +81,52 @@ void app_main(void){
     // Enable TWAI node
     ESP_ERROR_CHECK(twai_node_enable(sender_node));
     ESP_LOGI(TAG, "TWAI Sender started successfully");
-  
+    ESP_LOGI(TAG, "Sending messages with IDs: 0x%03X (data), 0x%03X (heartbeat)",  TWAI_DATA_ID, TWAI_HEARTBEAT_ID);
+
     while (1) {
+        // Send heartbeat message
         uint64_t timestamp = esp_timer_get_time();
-
-	uint8_t data_buffer[] = {1, 2, 3, 4};
-
         twai_frame_t tx_frame = {
-            .header.id = TWAI_SENSOR_1,
-            .buffer = (uint8_t *) &data_buffer,
-            .buffer_len = sizeof(data_buffer),
+            .header.id = TWAI_HEARTBEAT_ID,
+            .buffer = (uint8_t *) &timestamp,
+            .buffer_len = sizeof(timestamp),
         };
         ESP_ERROR_CHECK(twai_node_transmit(sender_node, &tx_frame, 500));
-        
-        // Log via ESP_LOGI
-        ESP_LOGI(TAG, "Sending Sensor 1 message: %" PRIu64, timestamp);
-        
-        // Log via printf para visualização clara
-        printf("[TWAI TX] Heartbeat - ID: 0x%03X | Timestamp: %" PRIu64 " us\n", 
-               TWAI_HEARTBEAT_ID, timestamp);
-        
+        ESP_LOGI(TAG, "Sending heartbeat message: %lld", timestamp);
         ESP_ERROR_CHECK(twai_node_transmit_wait_all_done(sender_node, -1)); // -1 means wait forever
-        
-        
-        vTaskDelay(pdMS_TO_TICKS(100));
+
+        // Send burst data messages every 10 seconds
+        if ((timestamp / 1000000) % 10 == 0) {
+            int num_frames = howmany(TWAI_DATA_LEN, TWAI_FRAME_MAX_LEN);
+            twai_sender_data_t *data = (twai_sender_data_t *)calloc(num_frames, sizeof(twai_sender_data_t));
+            assert(data != NULL);
+            ESP_LOGI(TAG, "Sending packet of %d bytes in %d frames", TWAI_DATA_LEN, num_frames);
+            for (int i = 0; i < num_frames; i++) {
+                data[i].frame.header.id = TWAI_DATA_ID;
+                data[i].frame.buffer = data[i].data;
+                data[i].frame.buffer_len = TWAI_FRAME_MAX_LEN;
+                memset(data[i].data, i, TWAI_FRAME_MAX_LEN);
+                ESP_ERROR_CHECK(twai_node_transmit(sender_node, &data[i].frame, 500));
+            }
+
+            // Insert an emergency frame with high priority
+            // This frame will be transmitted before the queue remaining data frames
+            twai_frame_t emergency_frame = {
+                .header.id = TWAI_EMERGENCY_ID,
+            };
+            ESP_LOGI(TAG, "Inserting Emergency message: 0x%03X", TWAI_EMERGENCY_ID);
+            ESP_ERROR_CHECK(twai_node_transmit(sender_node, &emergency_frame, 500));
+
+            // Frames mounted, wait for all frames to be transmitted
+            ESP_ERROR_CHECK(twai_node_transmit_wait_all_done(sender_node, -1));
+            free(data);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
         twai_node_status_t status;
         twai_node_get_info(sender_node, &status, NULL);
         if (status.state == TWAI_ERROR_BUS_OFF) {
             ESP_LOGW(TAG, "Bus-off detected");
-            printf("[TWAI TX] ERROR: Bus-off detected! Exiting...\n");
             return;
         }
     }
